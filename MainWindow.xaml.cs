73dd10be995724c279ce9deaa468aa8803b7a877
srirayavarapu <srirayavarapu@gmail.com>
2024-01-19 21:14:34 +0530

    Draft Verson

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,11 +7,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Core;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -24,15 +27,58 @@ namespace MageWin
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private Windows.Networking.Sockets.MessageWebSocket webSocket;
         public MainWindow()
         {
             this.InitializeComponent();
-            this.AppWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 300, 600));
+            this.AppWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 800, 800));
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            TextGrid.Visibility = Visibility.Visible;
+            ChannelButton.Visibility = Visibility.Collapsed;
+        }
+        private void AppBar_Closing(object sender, object e)
+        {
+            TextGrid.Visibility = Visibility.Collapsed;
+            ChannelButton.Visibility = Visibility.Visible;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var res = await client.GetStringAsync("https://api.mage.stream/wsinit/channelid?channelId=" + ChannelText.Text);
+            webSocket = new Windows.Networking.Sockets.MessageWebSocket();
+            await webSocket.ConnectAsync(new Uri("wss://api.mage.stream/wsinit/channelid/" + res + "/connect"));
+            webSocket.Control.MessageType = Windows.Networking.Sockets.SocketMessageType.Utf8;
+
+            webSocket.MessageReceived += WebSocket_MessageReceived;
+            webSocket.Closed += WebSocket_Closed;
+
+        }
+
+        private void WebSocket_Closed(Windows.Networking.Sockets.IWebSocket sender, Windows.Networking.Sockets.WebSocketClosedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WebSocket_MessageReceived(Windows.Networking.Sockets.MessageWebSocket sender, Windows.Networking.Sockets.MessageWebSocketMessageReceivedEventArgs args)
+        {
+            try
+            {
+                using (DataReader dataReader = args.GetDataReader())
+                {
+                    dataReader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                    string message = dataReader.ReadString(dataReader.UnconsumedBufferLength);
+                    Debug.WriteLine("Message received from MessageWebSocket: " + message);
+                    this.webSocket.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Windows.Web.WebErrorStatus webErrorStatus = Windows.Networking.Sockets.WebSocketError.GetStatus(ex.GetBaseException().HResult);
+            }
         }
     }
 }
