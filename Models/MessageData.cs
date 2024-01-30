38703e4a498a723a7d831ea2d82b49209c383d55
srirayavarapu <srirayavarapu@gmail.com>
2024-01-30 07:59:25 +0530

    All fixes done

---
+++ Models/MessageData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MageWin.Models
+{
+    public class MessageData
+    {
+        public string eventName { get; set; }
+        public User user { get; set; }
+        public long timestamp { get; set; }
+        public string message { get; set; }
+    }
+
+    public class User
+    {
+        public int userId { get; set; }
+        public string username { get; set; }
+    }
+}
